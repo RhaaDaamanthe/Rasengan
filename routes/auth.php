5c9cdbe42564ee1914d294f1ff1a7c2921a87948
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-22 09:39:06 +0200

    Fichier routage Auth

---
+++ routes/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Controller\Authentification\InscriptionController;
+use App\Controller\Authentification\ConnexionController;
+use App\Controller\Authentification\LogoutController;
+
+$router->get('/inscription', InscriptionController::class);
+$router->get('/connexion', ConnexionController::class);
+$router->post('/logout', LogoutController::class);
