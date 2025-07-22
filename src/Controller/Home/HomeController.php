030f7d09a26d1ae6806bbde1d206d79a911c5d9f
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-22 10:27:51 +0200

    Update HomeController.php

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Home;
 
 class HomeController
 {
-    public function __invoke(): void
+   public function __invoke(): void
     {
-        //page d'accueil du site
-         require_once __DIR__ . '/../../../public/Html/Home/home.php';
+        session_start();
+
+        $pseudo = $_SESSION['pseudo'] ?? null;
+
+        require_once __DIR__ . '/../../../public/Html/Home/home.php';
     }
 }
