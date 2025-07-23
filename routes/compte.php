68740ee809c13bbbbf87f2f892a85a89963f2c50
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-23 11:35:32 +0200

    Update compte.php (fichier de routage)

--- routes/compte.php
+++ routes/compte.php
@@ -4,3 +4,7 @@ use App\Controller\Compte\AccountController;
 
 //route pour accéder au profil de l'utilisateur
 $router->get('/compte/:id', AccountController::class);
+$router->post('/compte/update', UpdateAccountController::class);
+
+//route pour accéder au profil d'un utilisateur en read-only
+$router->get('/compte/:id', AccountViewerController::class);
