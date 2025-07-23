5f38c81ecc65bcc9be4b5d8570a7ebfbea3f4edb
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-23 08:32:26 +0200

    Update collection.php (fichier de routage)

--- routes/collection.php
+++ routes/collection.php
@@ -1,5 +1,16 @@
 <?php
 
 use App\Controller\Collection\CollectionController;
+use App\Controller\Joueur\PlayerController;
+use App\Controller\Joueur\PlayerAnimeController;
+use App\Controller\Joueur\PlayerFilmController;
 
+//collection générale, voir l'ensemble des joueurs
 $router->get('/collection', CollectionController::class);
+
+
+//collection concernant les joueurs individuellement
+$router->get('/collection/joueur/:id', PlayerController::class);
+$router->get('/collection/joueur/:id/anime', PlayerAnimeController::class);
+$router->get('/collection/joueur/:id/film', PlayerFilmController::class);
+
