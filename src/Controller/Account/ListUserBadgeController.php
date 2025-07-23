1342d2aae6648d3decdf46fa6cc83f228703d07f
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-23 15:04:59 +0200

    Create ListUserBadgeController.php

---
+++ src/Controller/Account/ListUserBadgeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Badge;
+
+use App\Controller\AbstractController;
+use App\Repository\BadgeRepository;
+use App\Database\DBConnexion;
+
+class ListUserBadgeController extends AbstractController
+{
+    public function __invoke(): void
+    {
+        session_start();
+        $this->requireLogin();
+
+        $userId = $_SESSION['user_id'];
+
+        $pdo = DBConnexion::getOrCreateInstance()->getPdo();
+        $repo = new BadgeRepository($pdo);
+        $badges = $repo->getBadgesByUser($userId);
+
+        require_once __DIR__ . '/../../../public/Html/Badges/user_badges.php';
+    }
+}
