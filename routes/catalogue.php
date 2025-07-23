f413696939b01e01449a35e18977b9b47acc99e3
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-23 08:44:03 +0200

    Update catalogue.php (fichier de routage)

--- routes/catalogue.php
+++ routes/catalogue.php
@@ -1,7 +1,22 @@
 <?php
 
 use App\Controller\Catalogue\CatalogueController;
+use App\Controller\Catalogue\CatalogueAnimeController;
+use App\Controller\Catalogue\CatalogueAnimeMythiqueController;
+use App\Controller\Catalogue\CatalogueAnimeMythiqueDetailController;
+use App\Controller\Catalogue\CatalogueFilmController;
+use App\Controller\Catalogue\CatalogueFilmMythiqueController;
+use App\Controller\Catalogue\CatalogueFilmMythiqueDetailController;
 
+//renvoie sur la page de sélection
 $router->get('/catalogue', CatalogueController::class);
-$router->get('/catalogue/animes', CartesAnimeController::class);
 
+//affiche le catalogue des animes
+$router->get('/catalogue/anime', CatalogueAnimeController::class);
+$router->get('/catalogue/anime/mythique', CatalogueAnimeMythiqueController::class);
+$router->get('/catalogue/anime/mythique/:id', CatalogueAnimeMythiqueDetailController::class);
+
+//affiche le catalogue des films
+$router->get('/catalogue/film', CatalogueFilmController::class);
+$router->get('/catalogue/film/mythique', CatalogueFilmMythiqueController::class);
+$router->get('/catalogue/film/mythique/:id', CatalogueFilmMythiqueDetailController::class);
