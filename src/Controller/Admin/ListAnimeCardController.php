39acc0bb042a2aa6381c3039001dacb14acd7a95
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-24 16:03:43 +0200

    feat : ListAnimeCardController.php

--- src/Controller/Admin/ListAnimeCardController.php
+++ src/Controller/Admin/ListAnimeCardController.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Controller\Admin;
+
+use App\Controller\AbstractController;
+use App\Database\DBConnexion;
+use Repository\CarteAnimeRepository;
+
+class ListAnimeCardController extends AbstractController
+{
+    public function __invoke(): void
+    {
+        session_start();
+        $this->requireLogin();
+        $this->requireAdmin();
+
+        $pdo = DBConnexion::getOrCreateInstance()->getPdo();
+        $repo = new CarteAnimeRepository($pdo);
+        $cartes = $repo->getAllCartesWithRarityInfo();
+
+        require_once __DIR__ . '/../../../public/Html/admin/list-anime-cards.php';
+    }
+}
