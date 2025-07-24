48d37fdd94c4234164d4b93744e99c88aae704ac
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-24 15:46:41 +0200

    feat: CreateAnimeCardFormController.php

--- src/Controller/Admin/CreateAnimeCardFormController.php
+++ src/Controller/Admin/CreateAnimeCardFormController.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Controller\Admin;
+
+use App\Controller\AbstractController;
+use App\Database\DBConnexion;
+use App\Repository\AnimeRepository;
+
+class CreateAnimeCardFormController extends AbstractController
+{
+    public function __invoke(): void
+    {
+        session_start();
+        $this->requireLogin();
+        $this->requireAdmin();
+
+        $pdo = DBConnexion::getOrCreateInstance()->getPdo();
+        $animeRepo = new AnimeRepository($pdo);
+        $animes = $animeRepo->findAll();
+
+        require_once __DIR__ . '/../../../public/Html/admin/anime-card-form.php';
+    }
+}
