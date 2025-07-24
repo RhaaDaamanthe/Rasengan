68819bfedc6dff8e517817ba181868c57ff4c5c7
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-24 16:05:30 +0200

    feat : UpdateAnimeCardFormController.php

--- src/Controller/Admin/UpdateAnimeCardFormController.php
+++ src/Controller/Admin/UpdateAnimeCardFormController.php
@@ -6,19 +6,30 @@ use App\Controller\AbstractController;
 use App\Database\DBConnexion;
 use Repository\CarteAnimeRepository;
 
-class ListAnimeCardController extends AbstractController
+class UpdateAnimeCardFormController extends AbstractController
 {
-    public function __invoke(): void
+    public function __invoke(array $params): void
     {
         session_start();
         $this->requireLogin();
         $this->requireAdmin();
 
+        if (!isset($params['id']) || !is_numeric($params['id'])) {
+            http_response_code(400);
+            echo "ID invalide.";
+            exit;
+        }
+
         $pdo = DBConnexion::getOrCreateInstance()->getPdo();
         $repo = new CarteAnimeRepository($pdo);
-        $cartes = $repo->getAllCartesWithRarityInfo();
+        $carte = $repo->getByIdAnime((int)$params['id']);
 
-        require_once __DIR__ . '/../../../public/Html/admin/list-anime-cards.php';
+        if (!$carte) {
+            http_response_code(404);
+            echo "Carte non trouvée.";
+            exit;
+        }
+
+        require_once __DIR__ . '/../../../public/Html/admin/anime-card-edit-form.php';
     }
 }
-
