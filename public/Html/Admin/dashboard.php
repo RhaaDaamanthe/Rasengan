6b7226d005575515bc9a5be198d01642434cf5a9
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-24 15:27:39 +0200

    feat: Create dashboard.php

---
+++ public/Html/Admin/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Configuration Administrateur</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="/CSS/admin-dashboard.css">
+</head>
+<body>
+    <h1 style="text-align: center;">Espace Administrateur</h1>
+
+    <div class="admin-buttons">
+        <a href="/admin/anime-cartes" class="admin-btn">Gérer les cartes animes</a>
+        <a href="/admin/film-cartes" class="admin-btn">Gérer les cartes films</a>
+        <a href="/admin/badge" class="admin-btn">Gérer les badges</a>
+    </div>
+</body>
+</html>
