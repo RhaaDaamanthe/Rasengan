02ab86ba1bf1785c38b798603b17b29ed1018346
Loris01100 <131340597+Loris01100@users.noreply.github.com>
2025-07-25 08:52:47 +0200

    feat : Model Badge.php

---
+++ src/Model/Badge.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Model;
+
+class Badge
+{
+    private int $id;
+    private string $nom;
+    private ?string $description;
+    private ?string $imagePath;
+
+    public function __construct(int $id, string $nom, ?string $description = null, ?string $imagePath = null)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->description = $description;
+        $this->imagePath = $imagePath;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(?string $imagePath): void
+    {
+        $this->imagePath = $imagePath;
+    }
+}
