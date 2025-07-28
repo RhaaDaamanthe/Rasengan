f60b4ada62e00257a913baca40cfe54986c19a9b
Loris01100 <lolobach01100@gmail.com>
2025-07-28 16:29:10 +0200

    fix: Add comments to clarify the purpose of requireLogin, requireAdmin, and redirect methods in AbstractController

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 abstract class AbstractController
 {
+    //fonction qui permet d'obliger sur certaines pages à être connecté
+    //si l'utilisateur n'est pas connecté, il est redirigé vers la page de connexion
     protected function requireLogin(string $redirect = '/connexion'): void
     {
         if (!isset($_SESSION['user_id'])) {
@@ -14,6 +16,8 @@ abstract class AbstractController
         }
     }
 
+    //fonction qui permet de vérifier si l'utilisateur est un administrateur
+    //si l'utilisateur n'est pas un administrateur, il reçoit un message d'erreur lui interdisant l'accès
     protected function requireAdmin(): void
     {
         if (empty($_SESSION['is_admin']) || $_SESSION['is_admin'] != 1) {
@@ -23,6 +27,8 @@ abstract class AbstractController
         }
     }
 
+    //fonction qui permet de rediriger l'utilisateur vers une autre page
+    //elle prend en paramètre l'URL de redirection
     protected function redirect(string $url): void
     {
         header("Location: $url");
